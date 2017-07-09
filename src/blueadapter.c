6f628d242f8bea3096426ed4224486b8c1f4fc8b
Raúl Casanova Marqués <17550266+rcasanovama@users.noreply.github.com>
2017-07-09 14:43:34 +0200

    Added code and header files of the blue adapter.

---
+++ src/blueadapter.c
@@ -0,0 +1,1 @@
+#include "blueadapter.h"
---
+++ src/blueadapter.h
@@ -0,0 +1,9 @@
+#ifndef _BLUEADAPTER_H
+#define _BLUEADAPTER_H
+
+/**
+ * Author:  Raul Casanova Marques
+ * Date:    08/07/17
+ */
+
+#endif // _BLUEADAPTER_H
