f0d18ebd7d0a39fd9b976eb4086787a22a314b2a
Raúl Casanova Marqués <17550266+rcasanovama@users.noreply.github.com>
2017-07-15 18:03:27 +0200

    Added code and header files of the blue agent.

---
+++ src/bt-agent.h
@@ -0,0 +1,19 @@
+#ifndef _BLUETOOTH_AGENT_H
+#define _BLUETOOTH_AGENT_H
+
+/**
+ * Author:  Raul Casanova Marques
+ * Date:    15/07/17
+ */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _BLUETOOTH_AGENT_H
