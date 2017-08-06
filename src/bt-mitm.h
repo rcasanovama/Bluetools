e7ca8841159c249972bd4e6ead8bd0c13b9ee84a
Raúl Casanova Marqués <17550266+rcasanovama@users.noreply.github.com>
2017-08-06 16:36:18 +0200

    Added files for bluetooth mitm functions.

---
+++ src/bt-mitm.h
@@ -0,0 +1,22 @@
+#ifndef _BLUETOOTH_MAN_IN_THE_MIDDLE_H
+#define _BLUETOOTH_MAN_IN_THE_MIDDLE_H
+
+/**
+ * Author:  Raul Casanova Marques
+ * Date:    05/08/17
+ */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "bt-types.h"
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _BLUETOOTH_MAN_IN_THE_MIDDLE_H
