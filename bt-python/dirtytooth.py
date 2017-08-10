f135c7e878c156ddcd3ae810d46d446a7a63e2d5
Raúl Casanova Marqués <17550266+rcasanovama@users.noreply.github.com>
2017-08-10 20:30:51 +0200

    Migration from PyOBEX to nOBEX, some improvements and code refactor.

--- bt-python/dirtytooth.py
+++ bt-python/dirtytooth.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-import os
+import datetime
 import sys
+import time
 
 import bluetooth
-from PyOBEX import client, headers, responses
+from nOBEX import client, headers, responses
+
+device_address = '00:00:00:00:00:00'
 
 
 def write_file(filename, data):
-    f = open(filename, 'w')
-    f.write(data)
-    f.close()
+    with open(filename, 'w') as f:
+        f.write(data)
 
 
 def get_file(c, src_path, filename, book=True):
@@ -21,33 +23,36 @@ def get_file(c, src_path, filename, book=True):
 
     hdrs, data = c.get(src_path, header_list=[headers.Type(mimetype)])
     write_file(filename, data)
-    print('Done!\n')
+    print('Done!')
 
 
-device_address = '00:00:00:00:00:00'
-# device_address = '00:00:00:00:00:00'
-prefix = ''  # '/SIM1', 'SIM1', '/'
+def main():
+    svc = bluetooth.find_service(address=device_address, uuid='1130')
+    if len(svc) < 1:
+        sys.stderr.write('Failed to find PBAP service.\n')
+        sys.exit(1)
 
-svc = bluetooth.find_service(address=device_address, uuid='1130')
-if len(svc) < 1:
-    sys.stderr.write("Failed to find PBAP service.\n")
-    sys.exit(1)
+    channel = svc[0]['port']
+    c = client.Client(device_address, channel)
 
-channel = svc[0]['port']
-c = client.Client(device_address, channel)
+    # 796135f0-f0c5-11d8-0966-0800200c9a66
+    uuid = b'\x79\x61\x35\xf0\xf0\xc5\x11\xd8\x09\x66\x08\x00\x20\x0c\x9a\x66'
+    r = c.connect(header_list=[headers.Target(uuid)])
 
-# 796135f0-f0c5-11d8-0966-0800200c9a66
-uuid = b'\x79\x61\x35\xf0\xf0\xc5\x11\xd8\x09\x66\x08\x00\x20\x0c\x9a\x66'
-r = c.connect(header_list=[headers.Target(uuid)])
+    if not isinstance(r, responses.ConnectSuccess):
+        sys.stderr.write('Failed to connect to phone.\n')
+        sys.exit(1)
 
-if not isinstance(r, responses.Success):
-    sys.stderr.write("Failed to connect to phone.\n")
-    sys.exit(1)
+    date = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
 
-if not os.path.isfile('/home/pi/hacktooth/phonebooks/phonebook_' + device_address):
-    get_file(c, prefix + 'telecom/pb.vcf', 'phonebook_' + device_address)
+    # Phone book
+    get_file(c, 'telecom/pb.vcf', '%s_%s-UTC_phonebook' % (device_address, date))
 
-if not os.path.isfile('/home/pi/hacktooth/phonebooks/history_' + device_address):
-    get_file(c, prefix + 'telecom/cch.vcf', 'history_' + device_address)
+    # Call history
+    get_file(c, 'telecom/cch.vcf', '%s_%s-UTC_history' % (device_address, date))
 
-c.disconnect()
+    c.disconnect()
+
+
+if __name__ == '__main__':
+    main()
